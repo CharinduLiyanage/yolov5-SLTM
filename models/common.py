ea7629c620e49793f00dbe367bcef29c47663bd9
Charindu Liyanage <charinduliyanage@yahoo.com>
2023-05-03 23:59:30 +0530

    Update common.py

--- models/common.py
+++ models/common.py
@@ -275,7 +275,7 @@ class GhostBottleneck(nn.Module):
 
 
 class ChannelAttention(nn.Module):
-
+    # Channel Attention Module for CBAM
     def __init__(self, in_planes, ratio=16):
         super().__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
@@ -293,12 +293,11 @@ class ChannelAttention(nn.Module):
 
 
 class SpatialAttention(nn.Module):
-
+    # Spacial Attention for CBAM module
     def __init__(self, kernel_size=7):
         super().__init__()
         assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
         padding = 3 if kernel_size == 7 else 1
-        # (特征图的大小-算子的size+2*padding)/步长+1
         self.conv = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
         self.sigmoid = nn.Sigmoid()
 
@@ -314,7 +313,7 @@ class SpatialAttention(nn.Module):
 
 
 class CBAM(nn.Module):
-    # CSP Bottleneck with 3 convolutions
+    # CBAM module
     def __init__(self, c1, c2, ratio=16, kernel_size=7):  # ch_in, ch_out, number, shortcut, groups, expansion
         super().__init__()
         self.channel_attention = ChannelAttention(c1, ratio)
